cd2a51514ed97caa110a26f68ba7df3782be4050
Chris Shotton <voidneumann@gmail.com>
2020-06-03 20:33:51 +0100

    Added speed solution (#1)
    * Added speed solution
    
    * Fixing gradle test config
    
    * Add gradle wrapper
    
    * Make gradlew executable

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,10 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testImplementation("junit:junit:4.12")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.4.2")
 }
+
+tasks.test { useJUnitPlatform() }
 
 tasks {
     compileKotlin {
---
+++ src/main/kotlin/easy/twosum/Solution.kt
@@ -0,0 +1,52 @@
+package easy.twosum
+
+class Solution {
+    /**
+     * My original solution
+     *
+     * Size O(1)
+     * Speed O(n^2)
+     *
+     * Submit results
+     * --------------
+     * Runtime: 288 ms, faster than 26.42% of Kotlin online submissions for Two Sum.
+     * Memory Usage: 34.6 MB, less than 100.00% of Kotlin online submissions for Two Sum.
+     */
+    fun twoSum_size(nums: IntArray, target: Int): IntArray {
+        var startIndex = 0
+        var offset = 1
+        while ((startIndex + offset) < nums.size) {
+            if (nums[startIndex] + nums[startIndex + offset] == target) {
+                return intArrayOf(startIndex, startIndex + offset)
+            }
+            offset++
+            if (startIndex + offset == nums.size) {
+                startIndex++
+                offset = 1
+            }
+        }
+        throw IllegalArgumentException("No solution available")
+    }
+
+
+    /**
+     * Created after looking at provided Java solutions
+     *
+     * Size O(1)
+     * Speed O(n^2)
+     *
+     * Submit results
+     * --------------
+     * Runtime: 184 ms, faster than 83.55% of Kotlin online submissions for Two Sum.
+     * Memory Usage: 35 MB, less than 100.00% of Kotlin online submissions for Two Sum.
+     */
+    fun twoSum_speed(nums: IntArray, target: Int): IntArray {
+        val complementMap = HashMap<Int, Int>()
+        for (i in nums.indices) {
+            complementMap[nums[i]]?.let { return intArrayOf(it, i) }
+            complementMap[target - nums[i]] = i
+        }
+        throw IllegalArgumentException("No solution available")
+    }
+}
+
